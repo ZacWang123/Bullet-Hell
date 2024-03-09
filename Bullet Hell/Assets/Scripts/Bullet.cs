d03f87450badb506e3412ade465536a925ac1182
ZacWang123 <Zac.W2002@yahoo.com>
2024-03-09 21:26:23 +1000

    Pattern 4 and delaying functions

--- Bullet Hell/Assets/Scripts/Bullet.cs
+++ Bullet Hell/Assets/Scripts/Bullet.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 public class Bullet : MonoBehaviour
 {
     public Vector2 bulletDirection;
     public float bulletSpeed;
+    public float timePassed;
 
+    IEnumerator WaitForFunction(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        ChangeDirection();
+    }
+    
     void OnEnable()
     {
         Invoke("Destroy", 3.5f);
@@ -31,6 +39,19 @@ public class Bullet : MonoBehaviour
     void Destroy()
     {
         gameObject.SetActive(false);
+    }
+
+    public void DirectionDelay(float waitTime)
+    {
+        StartCoroutine(WaitForFunction(waitTime));
+    }
+
+    public void ChangeDirection()
+    {
+        float tempX = bulletDirection.x;
+        float tempY = bulletDirection.y;
+        bulletDirection.x = tempY * -1;
+        bulletDirection.y = tempX;
     }
 
     private void OnDisable()
--- Bullet Hell/Assets/Scripts/FireBullet.cs
+++ Bullet Hell/Assets/Scripts/FireBullet.cs
@@ -11,13 +11,13 @@ public class FireBullet : MonoBehaviour
     float bulDirY;
     float angle;
     float angleStep;
-    float timer;
 
     void Start()
     {
         //InvokeRepeating("Pattern1", 0f, 0.3f);
         //InvokeRepeating("Pattern2", 0f, 0.1f);
-        InvokeRepeating("Pattern3", 0f, 0.15f);
+        //InvokeRepeating("Pattern3", 0f, 0.15f);
+        InvokeRepeating("Pattern4", 0f, 0.1f);
     }
 
     void Pattern1()
@@ -63,7 +63,7 @@ public class FireBullet : MonoBehaviour
             bul.GetComponent<Bullet>().SetBulletDirection(bulDir);
         }
 
-        angle += 10f;
+        angle += 40f;
         if (angle > 360f)
         {
             angle = 0f;
@@ -87,13 +87,37 @@ public class FireBullet : MonoBehaviour
             bul.GetComponent<Bullet>().SetBulletDirection(bulDir);
         }
 
-        angle += 5f;
+        angle += 10f;
         if (angle > 360f)
         {
             angle = 0f;
         }
     }
 
+    void Pattern4()
+    {
+        for (int i = 0; i <= 2; i++)
+        {
+            bulDirX = transform.position.x + Mathf.Sin(((angle + 120f * i) * Mathf.PI) / 180f);
+            bulDirY = transform.position.y + Mathf.Cos(((angle + 120f * i) * Mathf.PI) / 180f);
+
+            Vector3 bulMoveVector = new Vector3(bulDirX, bulDirY, 0f);
+            Vector2 bulDir = (bulMoveVector - transform.position).normalized;
+
+            GameObject bul = BulletPool.bulletPoolInstance.GetBullet();
+            bul.transform.position = transform.position;
+            bul.transform.rotation = transform.rotation;
+            bul.SetActive(true);
+            bul.GetComponent<Bullet>().SetBulletDirection(bulDir);
+            bul.GetComponent<Bullet>().DirectionDelay(1f);
+        }
+
+        angle += 5f;
+        if (angle > 360f)
+        {
+            angle = 0f;
+        }
+    }
 
     void Update()
     {
